f44a5e39ec0c4e6d31db2151abd6f1f26b359088
Nielsington <declercqniels@hotmail.com>
2023-03-01 17:11:40 +0100

    Add all elements and ideas to container

--- client/fetchAPI.js
+++ client/fetchAPI.js
@@ -15,16 +15,45 @@ console.log(apiData);
 const ideaContainer = document.querySelector('.ideaContainer');
 
 for (let idea of apiData){
+    const ideaDiv = document.createElement('div');
+    ideaDiv.classList.add(`${idea.id}`);
+
     const title = document.createElement('h3');
+    
+    const trashCan = document.createElement('img');
+    trashCan.classList.add('trashCan');
+    trashCan.src = './trash.png';
+
     const description = document.createElement('p');
     const createTime = document.createElement('p');
     createTime.classList.add('timestamp');
+
+    const linebreak = document.createElement('br');
+    const horizontalRule = document.createElement('hr');
+    horizontalRule.style.borderColor = "black";
 
     title.innerText = idea.title;
     description.innerText = idea.description;
     createTime.innerText = idea.created_at;
 
-    ideaContainer.appendChild(title);
-    ideaContainer.appendChild(description);
-    ideaContainer.appendChild(createTime);
+    ideaContainer.appendChild(ideaDiv)
+    ideaDiv.appendChild(title);
+    ideaDiv.appendChild(trashCan);
+    ideaDiv.appendChild(description);
+    ideaDiv.appendChild(createTime);
+    ideaDiv.appendChild(linebreak);
+    ideaDiv.appendChild(horizontalRule);
+
+    trashCan.addEventListener('click', async(req, res) =>{
+        ideaContainer.removeChild(ideaDiv);
+        try{
+        const response = await fetch(`http://localhost:3000/delete-row/${idea.id}`);
+        const deleteRow = await response.json();
+        }catch(err){
+            throw err;
+        }
+    });
 }
+
+
+
